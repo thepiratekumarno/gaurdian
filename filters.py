75588c59a4efde55cc49fa1cc09488c942d531c2
Harsh Maurya <142773164+thepiratekumarno@users.noreply.github.com>
2025-08-03 08:35:42 +0530

    Delete filters.py

--- filters.py
+++
@@ -1,9 +0,0 @@
-from jinja2 import Environment
-
-def datetimeformat(value, format='%Y-%m-%d %H:%M'):
-    if value is None:
-        return ""
-    return value.strftime(format)
-
-def init_filters(env: Environment):
-    env.filters['datetimeformat'] = datetimeformat
